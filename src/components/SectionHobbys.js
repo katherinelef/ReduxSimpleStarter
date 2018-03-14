36e1847ccf2e8c7e11a1f6065f61bf5a4ada0124
katherinelef <katherine.lef@hotmail.com>
2018-03-14 09:58:47 -0500

    lista de canciones

--- src/components/SectionHobbys.js
+++ src/components/SectionHobbys.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const SectionHobbys = () => (
-    <section>
-        <img src="https://image.ibb.co/nfWyVH/fotoportafolio.jpg" />
-      <ul> 
-          <li>Nadar </li>
-          <li>Ir al cine </li>
-          <li>Ir a la playa </li>
-          <li>Escuchar música </li>
-      </ul>
-    </section>
-)
+const SectionHobbys = () => {
+
+  const  hobbys = [
+          { id: 1, name: "Nadar" },
+          { id: 2, name: "Escuchar música" },
+          { id: 3, name: "Ir al cine" },
+          { id: 3, name: "Viajar" }
+        ]
+
+        return (
+             <ul>
+              {hobbys.map(hobby =>
+                 <li key={hobby.id}>{hobby.name}</li>
+              )}
+            </ul>
+          );
+     }
+    
 
 export default SectionHobbys
